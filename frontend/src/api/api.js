3ad94bee2064c89f0724709fcbb56de743d5b639
Amna <socialcircle87@gmail.com>
2026-04-03 14:26:05 +0500

    / remove

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://restaurant-app-production-916b.up.railway.app/",
+  baseURL: "https://restaurant-app-production-916b.up.railway.app",
   // baseURL: "http://localhost:3000",
   withCredentials: true,
 });
