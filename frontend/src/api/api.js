e5f9989468e63ab285dae5732f19d125cb58d7aa
Amna <socialcircle87@gmail.com>
2026-04-03 13:42:51 +0500

    origin change

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://restaurant-app-production-916b.up.railway.app/",
+  // baseURL: "http://localhost:3000",
   withCredentials: true,
 });
 
